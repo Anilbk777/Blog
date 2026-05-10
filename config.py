6907da51aa9bf449c3baafbb50ef4edf18df3297
Anilbk777 <anilbk775@gmail.com>
2026-05-10 20:02:53 +0545

    image_utils.py added

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Setting(BaseSettings):
     secret_key: SecretStr
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+    max_upload_size_bytes: int = 5 * 1024 * 1024
 
 
 settings = Setting()  # type:ignore[call-arg] # Loaded from .env file
